e9a02f2f0753fca9f20d0cbd9102bda875466b73
root <root@The-Singularity.localdomain>
2021-08-17 11:57:38 -0400

    Updated RuleControls component using Material UI and functional logic

--- src/js/components/RuleControls.js
+++ src/js/components/RuleControls.js
@@ -1,60 +1,65 @@
-import React, { Component } from "react";
-import Select from 'react-select';
+import React, { useEffect, useState } from 'react';
+import find from 'lodash/find';
+import get from 'lodash/get';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 
-import PresetRules from "../engine/PresetRules"
+import PresetRules from '../engine/PresetRules';
 
-class RuleControls extends Component {
-    constructor(props){
-        super(props);
+const RuleControls = ({ actions: { selectRule }, selectedRule: initialRule }) => {
+    const [selectedRule, setSelectedRule] = useState(initialRule);
 
-        this.state = {
-            selectedRule: props.selectedRule
-        }
+    useEffect(() => setSelectedRule(initialRule), [initialRule]);
 
-        this.handleRuleSelection = this.handleRuleSelection.bind(this);
+    const handleRuleSelection = (event) => {
+        const selectedValue = get(event, 'target.value');
+        const newRule = find(PresetRules, { value: selectedValue });
+
+        selectRule(newRule);
     }
 
-    handleRuleSelection(rule){
-        this.props.actions.selectRule(rule);
-    }
-
-    render(){
-        return (
-            <div id="rule-controls">
-                <div className="rule-select-container">
-                    <Select className="rule-selector"
-                        value={this.props.selectedRule}
-                        onChange={this.handleRuleSelection}
+    const weightGrid = get(selectedRule, 'weights', []);
+    return (
+        <div id="rule-controls">
+            <div className="rule-select-container">
+                <Select className="rule-selector"
+                        value={selectedRule.value}
+                        onChange={handleRuleSelection}
                         options={PresetRules}
-                    />
+                >
+                    {
+                        PresetRules.map(presetRule => (
+                            <MenuItem value={presetRule.value}>{presetRule.label}</MenuItem>
+                        ))
+                    }
+                </Select>
+            </div>
+            <div>
+                <label>Survival</label>
+                <input id="rule-survival" type="text" value={selectedRule.survival} disabled/>
+                <label>Birth</label>
+                <input id="rule-birth" type="text" value={selectedRule.birth} disabled/>
+            </div>
+            <label>Tile Weights</label>
+            <div id="rule-weights" className="grid">
+                <div className="grid-row">
+                    <input id="top-left-weight" value={weightGrid[0][0]} className="tile" type="text" size="1" disabled/>
+                    <input id="top-center-weight" value={weightGrid[0][1]} className="tile" type="text" size="1" disabled/>
+                    <input id="top-right-weight" value={weightGrid[0][2]} className="tile" type="text" size="1" disabled/>
                 </div>
-                <div>
-                    <label>Survival</label>
-                    <input id="rule-survival" type="text" value={this.state.selectedRule.survival} onChange={ this.handleSurvivalChange } disabled/>
-                    <label>Birth</label>
-                    <input id="rule-birth" type="text" value={this.state.selectedRule.birth} onChange={ this.handleBirthChange } disabled/>
+                <div className="grid-row">
+                    <input id="left-weight" value={weightGrid[1][0]} className="tile" type="text" size="1" disabled/>
+                    <input id="center-weight" value={weightGrid[1][1]} className="tile" type="text" size="1" disabled/>
+                    <input id="right-weight" value={weightGrid[1][2]} className="tile" type="text" size="1" disabled/>
                 </div>
-                <label>Tile Weights</label>
-                <div id="rule-weights" className="grid">
-                    <div className="grid-row">
-                        <input id="top-left-weight" value={this.state.selectedRule.weights[0][0]} className="tile" type="text" size="1" disabled/>
-                        <input id="top-center-weight" value={this.state.selectedRule.weights[0][1]} className="tile" type="text" size="1" disabled/>
-                        <input id="top-right-weight" value={this.state.selectedRule.weights[0][2]} className="tile" type="text" size="1" disabled/>
-                    </div>
-                    <div className="grid-row">
-                        <input id="left-weight" value={this.state.selectedRule.weights[1][0]} className="tile" type="text" size="1" disabled/>
-                        <input id="center-weight" value={this.state.selectedRule.weights[1][1]} className="tile" type="text" size="1" disabled/>
-                        <input id="right-weight" value={this.state.selectedRule.weights[1][2]} className="tile" type="text" size="1" disabled/>
-                    </div>
-                    <div className="grid-row">
-                        <input id="bottom-left-weight" value={this.state.selectedRule.weights[2][0]} className="tile" type="text" size="1" disabled/>
-                        <input id="bottom-center-weight" value={this.state.selectedRule.weights[2][1]} className="tile" type="text" size="1" disabled/>
-                        <input id="bottom-right-weight" value={this.state.selectedRule.weights[2][2 ]} className="tile" type="text" size="1" disabled/>
-                    </div>
+                <div className="grid-row">
+                    <input id="bottom-left-weight" value={weightGrid[2][0]} className="tile" type="text" size="1" disabled/>
+                    <input id="bottom-center-weight" value={weightGrid[2][1]} className="tile" type="text" size="1" disabled/>
+                    <input id="bottom-right-weight" value={weightGrid[2][2 ]} className="tile" type="text" size="1" disabled/>
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    );
+};
 
 export default RuleControls;
