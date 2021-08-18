e7ee2c64a6fa20fcdabd76f266f20c590037d6fc
jriley <johnrileysoftware@gmail.com>
2021-08-18 11:42:35 -0400

    Added Simple Spec for Life Rules Engine

---
+++ src/js/engine/LifeEnginer.spec.js
@@ -0,0 +1,23 @@
+import LifeEngine from './LifeEngine';
+
+import PresetRules from '../../data/preset-rules.json';
+
+describe('Life Rules Engine', () => {
+    test('single iteration test (Conway)', () => {
+        const initialGrid = [
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+            [0, 1, 1, 0],
+            [1, 0, 0, 1],
+        ];
+
+        const expectedGrid = [
+            [0, 1, 1, 0],
+            [1, 0, 0, 1],
+            [1, 0, 0, 1],
+            [0, 1, 1, 0],
+        ];
+
+        expect(LifeEngine.run(initialGrid, PresetRules[0])).toEqual(expectedGrid);
+    });
+});
